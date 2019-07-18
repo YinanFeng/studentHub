bbc8d1a24552c636bcf9f9bf22a4a6f7d950b0c5
YinanFeng <z5167277@student.unsw.edu.au>
2019-07-18 17:05:48 +0800

    server: update register of studentChat

--- studentChatServer/studentChat/src/main/java/com/studentHub/controller/studentChatController.java
+++ studentChatServer/studentChat/src/main/java/com/studentHub/controller/studentChatController.java
@@ -1,5 +1,9 @@
 package com.studentHub.controller;
 
+import com.studentHub.websocket.TextWebSocketFrameHandler;
+import io.netty.channel.Channel;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +13,9 @@ public class studentChatController {
 
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
+        //test success
+//        Object msg = new TextWebSocketFrame("testetstetetette \n");
+//        TextWebSocketFrameHandler.channels.writeAndFlush(msg);
         return "hello "+name+"，this is first messge";
     }
 }
