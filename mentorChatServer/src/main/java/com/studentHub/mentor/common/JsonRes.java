a50cb6659025806830364f074b4b8c2b95eb67e0
YinanFeng <z5167277@student.unsw.edu.au>
2019-07-22 18:27:16 +0800

    server : add mentor controller

---
+++ mentorChatServer/src/main/java/com/studentHub/mentor/common/JsonRes.java
@@ -0,0 +1,18 @@
+package com.studentHub.mentor.common;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class JsonRes {
+
+    public static JSONObject resSuccess(){
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("flag", true);
+        return jsonObj;
+    }
+
+    public static JSONObject resError(){
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("flag", false);
+        return jsonObj;
+    }
+}
--- mentorChatServer/src/main/java/com/studentHub/mentor/controller/mentorChatController.java
+++ mentorChatServer/src/main/java/com/studentHub/mentor/controller/mentorChatController.java
@@ -1,6 +1,10 @@
 package com.studentHub.mentor.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.studentHub.mentor.common.ChannelMapper;
+import com.studentHub.mentor.common.JsonRes;
 import com.studentHub.mentor.websocket.TextWebSocketFrameHandler;
+import io.netty.channel.Channel;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,5 +21,37 @@ public class MentorChatController {
 //        TextWebSocketFrameHandler.channels.writeAndFlush(msg);
 
         return "hello "+name+"，this is first messge";
+    }
+
+
+    @RequestMapping("/mentorChat/receive")
+    public JSONObject receiveMessageFromMentor(@RequestParam String message, @RequestParam String mentorId) {
+
+        Channel cn = ChannelMapper.get(mentorId);
+        if(cn == null){
+            return JsonRes.resError();
+            //inform mentor this stu is not here
+        }
+
+        Object msg = new TextWebSocketFrame(message + "\n");
+        cn.writeAndFlush(msg);
+
+        return JsonRes.resSuccess();
+    }
+
+    //student leave, inform mentor
+    @RequestMapping("/mentorChat/leave")
+    public JSONObject mentorLeave(@RequestParam String mentorId) {
+        Channel cn = ChannelMapper.get(mentorId);
+        if(cn == null){
+            return JsonRes.resError();
+        }
+
+        cn.writeAndFlush(new TextWebSocketFrame("student already leave" + "\n"));
+
+        //need more test? what is different from ctx.close() is this ok?
+        cn.close();
+
+        return JsonRes.resSuccess();
     }
 }
